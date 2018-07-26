7cdd8ddfbc24c7f818d64d935453461ab908a599
jthomps86@gmail.com <jthomps86@gmail.com>
2018-07-26 20:25:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -35,7 +35,7 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array
 }
 function removeElementFromBeginningOfArray(array) {
-  array.slice(1)
+  elemearray.slice(1)
   return array
 }
 function destructivelyRemoveElementFromEndOfArray(array) {
